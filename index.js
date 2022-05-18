1030ddfb142d56be4bf11a343819f23e8c127e50
Luiz-Alvares <luiz.wandre@gmail.com>
2022-05-17 20:28:33 -0400

    teste metodos http

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Teste agora')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+        response.send('ok');
+    });
+
+app.listen(3000);
+
+//METODOS HTTP
+//POST - criar - Creat
+//GET - buscar, trazer - Read
+//PUT - atualizar - Update
+//DELETE - deletar, remover - Delete
+
+const usuarios = [{
+    nome:'Luiz',
+    idade: 31,
+}];
+
+
+
+app.get('/usuarios', (request, response) => {
+    response.json(usuarios);
+});
+
+app.listen(3000);
